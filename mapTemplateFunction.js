8229490a36b2421d81782945c21871cdce32be00
Hannah Elarabawy <helarabawy@ucla.edu>
2017-08-31 10:08:34 -0700

    CFTM V1 (Before Cleaning)

--- mapTemplateFunction.js
+++ mapTemplateFunction.js
@@ -1,72 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+	  <link rel="stylesheet" href="css/homeViewStyle.css">
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	  <title>Home View</title>
+	</head>
+<body>
 
-////////////////////////////////////////////////////////////////////////
-/////////////////////////// SETTING UP DOTS ////////////////////////////
-////////////////////////////////////////////////////////////////////////
+	<img class="logo centered-x" src="/Hansel_Test/views/Hansel-logo.png" alt="Hansel Logo">
+	<button id='travelGuides' class="btn-light-color centered-x" type="button"><span>Travel Guides > </span></button>
+  
 
- var centerDot = 1;
- var activeDot = 0;
- function moveDots(){
-     var dots = document.getElementsByClassName("place-dot");
-     
-     for (var i = 0; i < totalSpotCount; i++)
-    	 dots[i].className = "place-dot hidden";
-     
-     activeDot = index ;
-     
-     if (activeDot > 0 && activeDot < totalSpotCount - 1)
-         centerDot = activeDot;
-     else if (activeDot == 0)
-         centerDot = activeDot + 1;
-     else if (activeDot == totalSpotCount - 1)
-         centerDot = activeDot - 1;
-     
-     try{dots[centerDot-3].className = "place-dot show smaller";} catch(err){}
-     try{dots[centerDot-2].className = "place-dot show small";} catch(err){}
-     try{dots[centerDot-1].className = "place-dot show";} catch(err){}
-     try{dots[centerDot].className = "place-dot show";} catch(err){}
-     try{dots[centerDot+1].className = "place-dot show";} catch(err){}
-     try{dots[centerDot+2].className = "place-dot show small";} catch(err){}
-     try{dots[centerDot+3].className = "place-dot show smaller";} catch(err){}    
+    <script>
+    /// Travel Guides Button Functionality
+		document.getElementById('travelGuides').addEventListener('click', function () {
+			location.href = "/Hansel_Test/views/CFTMmenuView.jsp";
+		});
+    </script>
+    
+    <script src="https://hammerjs.github.io/dist/hammer.js"></script>
 
-     try{dots[activeDot].className = "place-dot show active";} catch(err){}
- }
- 
-//////////////////////////////////////////////////////////////////////////
-////////////////////////////JS FUNCTIONS ////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-function expand() {
-	document.getElementById("fixed-header").style.height = "100%";
-	document.getElementById("fixed-header").style.overflow = "auto";
-	document.getElementById("playlist-container").style.display = "block";
-	document.getElementById("title-container").style.display = "none";
-}
-
-function collapse(){
-	document.getElementById("fixed-header").style.height = "10%";
-	document.getElementById("fixed-header").style.overflow = "hidden";
-	document.getElementById("title-container").style.display = "block";
-	document.getElementById("playlist-container").style.display = "none";
-}
-
-
-function imagePopUp(){
-	document.getElementById("img-popup").style.display = "block";
-	document.getElementById("image-link").style.display = "none";
-}
-function imagePopDown(){
-	document.getElementById("img-popup").style.display = "none";
-	document.getElementById("image-link").style.display = "block";   
-}
-
-function navigate() {
-	// Forming link
-	var link = "https://www.google.com/maps?saddr=My+Location&daddr=" + yValues[index] + "," + xValues[index] + "&dirflg=w";
+	<div id="myElement"></div>
 	
-	if( (navigator.platform.indexOf("iPhone") != -1) 
-	|| (navigator.platform.indexOf("iPod") != -1)
-	|| (navigator.platform.indexOf("iPad") != -1))
-		window.open(link);
-	else
-		window.open(link);
-
-}
+	<style>
+		#myElement {
+		  background: white;
+		  height: 100%;
+		  text-align: center;
+		}
+	</style>
+   
+   <script> 
+		var myElement = document.getElementById('myElement');
+		
+		// create a simple instance
+		// by default, it only adds horizontal recognizers
+		var mc = new Hammer(myElement);
+		
+		// let the pan gesture support all directions.
+		// this will block the vertical scrolling on a touch-device while on the element
+		
+		// listen to events...
+		mc.on("panleft", function(ev) {
+			location.href = "/Hansel_Test/views/CFTMmenuView.jsp";
+		});
+	</script>
+	
+</body>
+</html>
